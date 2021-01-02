787b38ff1174dc4a20fadf0ad6253d5f23912db2
thegi_000 <bustard@wp.pl>
2021-01-02 15:41:29 +0100

    add mainapp

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,7 +2,29 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Test");
-        System.out.println("Tj");
-    }
-}
+        int pierwsza = 131;
+        int druga = 9;
+        int trzecia = 22;
+        int czwarta = 47;
+        int suma1 = pierwsza + druga;
+        int suma2 = trzecia + czwarta;
+
+
+        if(suma1 % 2 == 0){
+            System.out.println("parzysta");
+        } else {
+            System.out.println("nieparzysta");}
+
+        if(suma2 % 2 == 0){
+            System.out.println("parzysta");
+        } else {
+            System.out.println("nieparzysta");}
+
+        if (suma1 % 2 == 0 && suma2 % 2 == 0) {
+            System.out.println("obie sumy są parzyste");
+        } else {
+            System.out.println("jedna suma jest nieparzysta");
+        }
+
+
+    }};
